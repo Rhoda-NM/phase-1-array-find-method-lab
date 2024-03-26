8c73141fcde564ea07cc0b8cea030fee454e341c
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-27 00:11:00 +0300

    Using .find()

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+function superbowlWin(array) {
+    let win = array.find((wins) => wins.result == 'W');
+    if (win) {return win.year}
+
+}
+console.log(superbowlWin(record));
